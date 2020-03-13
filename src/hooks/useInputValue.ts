b143b5ecbe73a542a63047bbd157800bfa95ceae
Maarten van Ittersum <maarten@v-ittersum.nl>
2020-03-13 14:31:09 +0100

    Added useInputValue hook, upped to version 0.3.0

---
+++ src/hooks/useInputValue.ts
@@ -0,0 +1,22 @@
+import {useCallback, useContext, useState} from "react";
+import {FormContext} from "../contexts/FormContext";
+import {useEventListener} from "./useEventListener";
+import {CHANGE_EVENT} from "../events";
+
+export function useInputValue(fields: string[]): any[] {
+    const {getValue, listener} = useContext(FormContext);
+    const [values, setValues] = useState<any[]>(() => fields.map(field => getValue(field)));
+
+    const handleChange = useCallback((event: CustomEvent<any>) => {
+        const valueIndex = fields.indexOf(event.detail.name);
+        if (valueIndex !== -1) {
+            const newValues = [...values];
+            values[valueIndex] = event.detail.value;
+            setValues(newValues);
+        }
+    }, [fields]);
+
+    useEventListener(listener, CHANGE_EVENT, handleChange as EventListener);
+
+    return values;
+}
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import {Form} from "./components/Form";
 import * as events from "./events";
 import {useFormContext} from "./hooks/useFormContext";
 import {useInput} from "./hooks/useInput";
+import {useInputValue} from "./hooks/useInputValue";
 
 export {
   Form,
   useInput,
   useFormContext,
+  useInputValue,
   events
 };
