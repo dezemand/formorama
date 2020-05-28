e91863d6c9f458653323e8d717bcea7c7c9225d0
Maarten <m.van.ittersum@student.rug.nl>
2020-05-28 22:36:51 +0200

    `useInputValue` now works with subforms.

--- src/hooks/useInputValue.ts
+++ src/hooks/useInputValue.ts
@@ -1,8 +1,8 @@
 import {useCallback, useContext, useState} from "react";
 import {FormContext} from "../contexts/FormContext";
 import {CHANGE_EVENT, CustomChangeEvent} from "../events";
-import {FormHook, Path, UnparsedPath} from "../types";
-import {parsePath, pathEquals} from "../utils/path";
+import {FormHook, Path, PathNodeType, UnparsedPath} from "../types";
+import {parsePath, pathEquals, pathParentOf, setTreeValue} from "../utils/path";
 import {useEventListener} from "./useEventListener";
 
 export function useInputValue(fields: (UnparsedPath)[], form?: FormHook, uPath?: UnparsedPath): any[] {
@@ -16,14 +16,26 @@ export function useInputValue(fields: (UnparsedPath)[], form?: FormHook, uPath?:
   const [values, setValues] = useState<any[]>(() => fieldPaths.map(fieldPath => getValue(fieldPath)));
 
   const handleChange = useCallback((event: CustomChangeEvent) => {
+    const newValues = [...values];
+    let changed = false;
+
     for (const {path: valuePath, value} of event.detail.values) {
       for (const [index, fieldPath] of fieldPaths.entries()) {
         if (pathEquals(valuePath, fieldPath)) {
-          const newValues = [...values];
           newValues[index] = value;
-          setValues(newValues);
+          changed = true;
+        } else if (pathParentOf(fieldPath, valuePath)) {
+          const subPath = valuePath.slice(fieldPath.length);
+          const tree = newValues[index] || (subPath[0][0] === PathNodeType.ARRAY_INDEX ? [] : {});
+          setTreeValue(tree, subPath, value);
+          newValues[index] = tree;
+          changed = true;
         }
       }
+    }
+
+    if (changed) {
+      setValues(newValues);
     }
   }, [fieldPaths, values]);
 
