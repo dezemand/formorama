0dbf39accb5a1dd6863f2df273fb71cacd62b2b2
Maarten <m.van.ittersum@student.rug.nl>
2020-05-29 02:35:05 +0200

    Added polyfill for CustomEvent and EventTarget.

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -2,6 +2,7 @@ import {useMemo, useRef, useState} from "react";
 import {BLUR_EVENT, CHANGE_EVENT, ChangeEventDetail, DO_SUBMIT_EVENT, FOCUS_EVENT, FocusBlurEventDetail, POST_CHANGE_EVENT} from "../events";
 import {ErrorObject, FormHook, FormHookType, Path, RootForm, RootFormFunctions} from "../types";
 import {addNullErrors, createChangesMap, createErrorsMap} from "../utils/changes";
+import {createCustomEvent, createEventTarget} from "../utils/events";
 import {getTreeValue, parsePath, pathEquals, ROOT_PATH, setTreeValue} from "../utils/path";
 import {useFormIO} from "./useFormIO";
 
@@ -13,7 +14,7 @@ export function useForm<Values>({validate}: UseFormParameters<Values> = {}): For
   const values = useRef<Values>({} as Values);
   const errors = useRef<ErrorObject>([]);
   const focusing = useRef<Path | null>(null);
-  const target = useRef<EventTarget>(new EventTarget());
+  const target = useRef<EventTarget>(createEventTarget());
   const [submitting, setSubmitting] = useState(false);
   const path = useMemo(() => ROOT_PATH, []);
 
@@ -23,7 +24,7 @@ export function useForm<Values>({validate}: UseFormParameters<Values> = {}): For
     const errorMap = createErrorsMap([], errorObject);
     errors.current = addNullErrors(errorMap, errors.current);
 
-    target.current.dispatchEvent(new CustomEvent<ChangeEventDetail>(CHANGE_EVENT, {
+    target.current.dispatchEvent(createCustomEvent<ChangeEventDetail>(CHANGE_EVENT, {
       detail: {
         values: [],
         errors: errors.current
@@ -48,14 +49,14 @@ export function useForm<Values>({validate}: UseFormParameters<Values> = {}): For
     change(uPath, value) {
       const path = parsePath(uPath);
       values.current = setTreeValue(values.current, path, value);
-      target.current.dispatchEvent(new CustomEvent<ChangeEventDetail>(CHANGE_EVENT, {
+      target.current.dispatchEvent(createCustomEvent<ChangeEventDetail>(CHANGE_EVENT, {
         detail: {
           values: createChangesMap(path, value),
           errors: []
         }
       }));
       window.setTimeout(() => {
-        target.current.dispatchEvent(new CustomEvent<ChangeEventDetail>(POST_CHANGE_EVENT, {
+        target.current.dispatchEvent(createCustomEvent<ChangeEventDetail>(POST_CHANGE_EVENT, {
           detail: {
             values: createChangesMap(path, value),
             errors: []
@@ -67,7 +68,7 @@ export function useForm<Values>({validate}: UseFormParameters<Values> = {}): For
     focus(uPath) {
       const path = parsePath(uPath);
       focusing.current = path;
-      target.current.dispatchEvent(new CustomEvent<FocusBlurEventDetail>(FOCUS_EVENT, {
+      target.current.dispatchEvent(createCustomEvent<FocusBlurEventDetail>(FOCUS_EVENT, {
         detail: {
           path
         }
@@ -79,7 +80,7 @@ export function useForm<Values>({validate}: UseFormParameters<Values> = {}): For
       if (focusing.current && pathEquals(path, focusing.current)) {
         focusing.current = null;
 
-        target.current.dispatchEvent(new CustomEvent<FocusBlurEventDetail>(BLUR_EVENT, {
+        target.current.dispatchEvent(createCustomEvent<FocusBlurEventDetail>(BLUR_EVENT, {
           detail: {
             path
           }
@@ -88,7 +89,7 @@ export function useForm<Values>({validate}: UseFormParameters<Values> = {}): For
     },
 
     submit() {
-      target.current.dispatchEvent(new CustomEvent(DO_SUBMIT_EVENT));
+      target.current.dispatchEvent(createCustomEvent(DO_SUBMIT_EVENT));
     },
 
     async getValidationResult(valuesObject) {
