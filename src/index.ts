e896085e707097e732c555f672af56e03c038e39
Maarten van Ittersum <maarten@v-ittersum.nl>
2020-07-16 13:38:36 +0200

    Forgot to export useSubmitting

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
+// Events object
 export * as events from "./events";
-export {Form} from "./components/Form";
-export {SubForm} from "./components/SubForm";
+
+// Components
 export {ArrayForm} from "./components/ArrayForm";
 export {ArrayFormItem} from "./components/ArrayFormItem";
 export {ArrayFormItems} from "./components/ArrayFormItems";
+export {Form} from "./components/Form";
+export {SubForm} from "./components/SubForm";
+
+// Contexts
 export {FormConsumer} from "./contexts/FormContext";
+
+// Exceptions
 export {SubmissionError} from "./exceptions/SubmissionError";
+
+// Hooks
+export {useForm} from "./hooks/useForm";
 export {useFormContext} from "./hooks/useFormContext";
 export {useInput} from "./hooks/useInput";
 export {useInputValue} from "./hooks/useInputValue";
-export {useForm} from "./hooks/useForm";
+export {useSubmitting} from "./hooks/useSubmitting";
